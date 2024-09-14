9dbe0f89b1e22220ad7e07c0dd1311a64e9eea7f
Verminas <veremeeva96@gmail.com>
2024-09-14 15:42:26 +0300

    feat: add environment for my project

--- src/api/todolistsApi.ts
+++ src/api/todolistsApi.ts
@@ -1,11 +1,10 @@
 import axios from "axios";
-import { apiKey } from "./apiKey";
 
 export const instance = axios.create({
-  baseURL: "https://social-network.samuraijs.com/api/1.1/",
+  baseURL: process.env.REACT_APP_BASE_URL,
   withCredentials: true,
   headers: {
-    "API-KEY": apiKey,
+    "API-KEY": process.env.REACT_APP_API_KEY,
   },
 });
 
