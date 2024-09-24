0933fb8f55cdab3ea72a24a7cfb76959e4021d5b
Verminas <veremeeva96@gmail.com>
2024-09-24 16:29:01 +0300

    delete: common utils tryCatch func

--- src/common/utils/index.ts
+++ src/common/utils/index.ts
@@ -1,6 +1,2 @@
-export { changeEntityStatus, type Entity } from "./changeEntityStatus";
 export { createAppAsyncThunk } from "./createAppAsyncThunk";
 export { getTaskUpdateModel } from "./getTaskUpdateModel";
-export { handleServerAppError } from "./handleServerAppError";
-export { handleServerNetworkError } from "./handleServerNetworkError";
-export { thunkTryCatch } from "./thunkTryCatch";
